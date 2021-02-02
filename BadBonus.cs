50729680276302308e13d0cb4184c98a366a44a1
Lada <age.la.eva@mail.ru>
2021-02-02 13:54:35 +0300

    Homework 4

---
+++ BadBonus.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+
+namespace Labyrinth
+{
+    public class BadBonus : InteractiveObject, IFlay, IRotation
+    {
+        private float _lengthFlay;
+        private float _speedRotation;
+
+        public event Action<string, Color> OnCaughtPlayerChange = delegate(string str, Color color){ };
+
+        private void Awake()
+        {
+            _lengthFlay = Random.Range(1.0f, 5.0f);
+            _speedRotation = Random.Range(10.0f, 50.0f);
+        }
+
+        protected override void Interaction()
+        {
+            OnCaughtPlayerChange.Invoke(gameObject.name, _color);
+        }
+        public override void Execute()
+        {
+            if (!IsInteractable) { return; }
+            Flay();
+            Rotation();
+        }
+
+        public void Flay()
+        {
+            transform.localPosition = new Vector3(transform.localPosition.x,
+             Mathf.PingPong(Time.time, _lengthFlay),
+             transform.localPosition.z);
+        }
+
+        public void Rotation()
+        {
+            transform.Rotate(Vector3.up * (Time.deltaTime * _speedRotation), Space.World);
+        }
+    }
+}
+
+
---
+++ VictoryPosition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using static UnityEngine.Random;
+
+
+namespace Labyrinth
+{
+    public sealed class VictoryPosition : InteractiveObject, IFlicker
+    {
+        private Material _material;
+        //public event Action<int> OnPointChange = delegate (int i) { };
+
+        private void Awake()
+        {
+            _material = GetComponent<Renderer>().material;
+        }
+
+        public override void Execute()
+        {
+            
+        }
+        public void Flicker()
+        {
+            _material.color = new Color(_material.color.r, _material.color.g, _material.color.b,
+                Mathf.PingPong(Time.time, 1.0f));
+        }
+        protected override void Interaction()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
